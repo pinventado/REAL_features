fc88f52573c9e15d671091c8fad3d7bcc0cdce36
Paul Inventado <p.inventado@waagle.com>
2014-05-22 11:29:48 -0400

    Fixed special cases in parsing

--- category.py
+++ category.py
@@ -1,19 +1,37 @@
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import requests
-import nltk
+from HTMLParser import HTMLParser
+import requests, nltk, json, urllib2, os.path
 #nltk.download()
 stoplist = stopwords.words("english")
+stoplist.append("so")
+stoplist.append("tell")
 lemmatizer = WordNetLemmatizer()
+parser = HTMLParser()
+mapfile = 'wordmap.json'
 
 class WordCategoryChecker:
-	def __init__(self):
-		self.word_map = {}
+	def __init__(self, mapfile_batch=10):
+		self.mapfile_batch = mapfile_batch
+		self.batch_ctr = 0
+		if os.path.isfile(mapfile):
+			with open(mapfile, 'r') as m_file:
+				self.word_map = json.load(m_file)
+		else:
+			self.word_map = {}
+
+	def store_mapfile(self):
+		with open(mapfile,'w') as m_file:
+			json.dump(self.word_map,m_file)
 
 	def add_mapping(self, word, category, state):
 		if not word in self.word_map:
 			self.word_map[word]={}
+			self.batch_ctr +=1	
 		self.word_map[word][category]=state
+		if self.batch_ctr == self.mapfile_batch:
+			self.store_mapfile()
+			self.batch_ctr = 0
 
 	def check(self, word, category):
 		if word == category:
@@ -22,48 +40,57 @@ class WordCategoryChecker:
 		if word in self.word_map and category in self.word_map[word]:
 			#print "L"
 			return self.word_map[word][category]
-		else:			
-			result = requests.get("http://conceptnet5.media.mit.edu/data/5.2/search?limit=1&rel=/r/IsA&start=/c/en/"+word+"&end=/c/en/"+category)
-			data = result.json()
+		else:
 			found = False
-			#print word, len(data['edges'])
-			#print data
-			for edges in data['edges']:
-				#print word, edges
-				start = edges['start'].replace("/c/en/","")#.split("_")
-				end = edges['end'].replace("/c/en/","")#.split("_")
-				#print "[",start, end
-				if edges['rel'] == '/r/IsA' and word == start and category == end:# and start == word and end == category:
-					#print edges
-					found = True
-					break
-		self.add_mapping(word, category, found)	
-		return found
+			if len(word)<100:
+				result = requests.get("http://conceptnet5.media.mit.edu/data/5.2/search?limit=1&filter=core&rel=/r/IsA&start=/c/en/"+word+"&end=/c/en/"+category)
+
+				data = result.json()
+				
+				#print word, len(data['edges'])
+				#print data
+				for edges in data['edges']:
+					#print word, edges
+					start = edges['start'].replace("/c/en/","")#.split("_")
+					end = edges['end'].replace("/c/en/","")#.split("_")
+					weight = float(edges['weight'])
+					#print "[",start, end
+					if edges['rel'] == '/r/IsA' and word == start and category == end and weight > 2:# and start == word and end == category:
+						#print edges
+						found = True
+						break
+				self.add_mapping(word, category, found)	
+			return found
 
 class SentenceCategoryChecker:
-	def __init__(self):
+	def __init__(self, isHTML=False):
 		self.word_check = WordCategoryChecker()
+		self.isHTML = isHTML
 
 	def check(self, sentence, category_list):
+		if self.isHTML:
+			sentence = nltk.clean_html(parser.unescape(urllib2.unquote(sentence).decode('utf-8', errors='ignore')))
+
+		#print sentence
 		sentence = ''.join(e for e in sentence if e.isalnum() or e.isspace())
 		#print sentence 
 		result = {}
 		for category in category_list:
 			result[category]= False
-		for word in sentence.split(" "):
-			if not word in stoplist:
-				word = lemmatizer.lemmatize(word).lower()
+		for word in sentence.split(" "):			
+			word = lemmatizer.lemmatize(word.lower())
+			if not word in stoplist and len(word)>1:				
 				#print word
 				for category in category_list:					
 					if self.word_check.check(word, category):
 						result[category] = True
-						#print word, category
+						print word, category
 				#print word	
 		return result
 
 #sc = SentenceCategoryChecker()
 #print sc.check("if there are 2 dogs inside the van, and one got lost. How many dogs would be left?",['car','animal'])
-
+#print sc.check("A sugar cookie recipe calls for 3 1/2 cups of sugar to make 6 dozen cookies. Based on the recipe, how many cups of sugar must be used to make 20 dozen sugar cookies?",['car'])
 
 '''
 cc = CategoryChecker()
--- driver.py
+++ driver.py
@@ -1,14 +1,65 @@
-from HTMLParser import HTMLParser
 from category import SentenceCategoryChecker
-import re
+import re, csv, nltk
 
-def clean_str(str_in):
+'''def clean_str(str_in):
 	str_in = re.sub('<[^>]*>', '', str_in)
 	return parser.unescape(str_in)
+'''
+#sample = clean_str("<p>Doc Worker is a regular customer at the Waterfront Coffee Shop. The manager has figured that Doc&rsquo;s probability of ordering ham is 0.8; and eggs, 0.65. What is the probability that (leave your answer as a percent, WITHOUT the %)</p><p>e)&nbsp; He orders at least one, either ham or eggs?<strong> </strong></p>")
+sc = SentenceCategoryChecker(isHTML=True)
+#categories = ['car','animal']
+categories = ['car','animal','person','sport','place','object','food','subject']
+header = ["Question"]
+#line = "Question, "
+#first = True   
+for categ in categories:
+	header.append(categ)
+#print header
+#		out = categ
+#		if first:
+#			first = False
+#		else:
+#			out = ", "+categ 
+#		line += out 
+#print line
 
-parser = HTMLParser()
-sample = clean_str("<p>Doc Worker is a regular customer at the Waterfront Coffee Shop. The manager has figured that Doc&rsquo;s probability of ordering ham is 0.8; and eggs, 0.65. What is the probability that (leave your answer as a percent, WITHOUT the %)</p><p>e)&nbsp; He orders at least one, either ham or eggs?<strong> </strong></p>")
-sc = SentenceCategoryChecker()
-categories = ['car','animal']
+#with open("temp.csv","r") as f:
+#	data = f.readlines()
+
+csv_file = open("temp.csv","rU")
+out_file = open("out.csv","wb")
+reader = csv.reader(csv_file, delimiter=",")
+writer = csv.writer(out_file, dialect='excel')
+writer.writerow(header)
+for row in reader:
+	if len(row)>0:
+		cur_row = [row[0]]
+		#print cur_row
+		#sample = row[0]
+		#line = sample+","
+		'''line = '"'+nltk.clean_html(sample)+'", '
+		if line.strip() == '':
+			break'''
+		#first = True
+		results = sc.check(row[0], categories)
+		for item in categories:
+			cur_row.append(results[item])
+			#out = str(col[1])
+			#if first:
+			#	first = False
+			#else:
+			#	out = ", "+str(col[1])
+			#line+= out
+			#print item 		
+			#cur_row.append(col[1])
+		print cur_row
+		writer.writerow(cur_row)
+	
 #print sc.check("if there are 2 dogs inside the van, and one got lost. How many dogs would be left?",['car','animal'])
-print sc.check(sample,categories)
+'''ctr = 0
+with open('temp.csv','r') as f:
+	entry = f.readlines()
+	#print entry
+	ctr+=1
+	print sc.check(entry,categories)
+'''
